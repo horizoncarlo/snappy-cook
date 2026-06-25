91a31b194e72a212af3c987d9e5b5307eb99a9ea
horizoncarlo <carlo.gug@gmail.com>
2026-06-25 14:51:40 -0600

    PM2 script instead of screen

--- go-prod.sh
+++ go-prod.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-screen -d -m -S snappycook sh -c "json-server -p 2500 --watch --host 0.0.0.0 db.json"
-screen -r
+#screen -d -m -S snappycook sh -c "json-server -p 2500 --watch --host 0.0.0.0 db.json"
+#screen -r
+
+pm2 start json-server --name "snappycook" -- -p 2500 --watch --host 0.0.0.0 db.json
